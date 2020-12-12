1331de8bb6149f012f74701a3b0ffedeaeb0db49
Rutger van Beek <rdjvanbeek@gmail.com>
2020-12-12 10:23:18 +0100

    initial commit

---
+++ src/todoist_connection.py
@@ -0,0 +1,33 @@
+import todoist
+
+
+class TodoistConnection():
+
+    def __init__(self, token):
+        self.api = todoist.TodoistAPI(token)
+        self._sync()
+
+    @classmethod
+    def from_config_file(cls, config_file):
+        with open(config_file) as file:
+            token = file.read()
+        return cls(token)
+
+
+    @property
+    def projects(self):
+        return self.api.state['projects']
+
+    @property
+    def tasks(self):
+        return self.api.state['items']
+
+    @property
+    def uncompleted_tasks(self):
+        return self.filter_tasks(lambda task: task['checked']==0)
+
+    def filter_tasks(self, filter_function):
+        return filter(filter_function, self.api.state['items'])
+
+    def _sync(self):
+        self.api.sync()
