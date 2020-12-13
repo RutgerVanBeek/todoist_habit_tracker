f4e55070ac3ba03c97e6de607109551041c35d69
Rutger van Beek <rdjvanbeek@gmail.com>
2020-12-13 20:04:48 +0100

    write to file

--- scripts/number_of_uncompleted_tasks.py
+++ scripts/number_of_uncompleted_tasks.py
@@ -1,9 +1,13 @@
 from todoist_habit_tracker import todoist_connection
 import os
+import datetime
+
 if __name__ == '__main__':
     dirname = os.path.abspath(os.path.dirname(__file__))
-    print(dirname)
     filename = os.path.join(os.path.split(dirname)[0], '.config/.todoist.txt')
-    print(filename)
     todoist = todoist_connection.TodoistConnection.from_config_file(filename)
+    dumpfile = os.path.join(os.path.split(dirname)[0], '.log/log.txt')
     print(len(todoist.uncompleted_tasks))
+    try:
+        with open(dumpfile, 'w') as f:
+            f.write(str(datetime.now)+ '     ' + str(len(todoist.uncompleted_tasks)))
