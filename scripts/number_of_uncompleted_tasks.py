f650b0793f0e81bb62cad324605af4cf07f560ca
Rutger van Beek <rdjvanbeek@gmail.com>
2020-12-13 19:49:11 +0100

    small bug fix

--- scripts/number_of_uncompleted_tasks.py
+++ scripts/number_of_uncompleted_tasks.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     filename = os.path.join(os.path.split(dirname)[0], '.config/.todoist.txt')
     print(filename)
     todoist = todoist_connection.TodoistConnection.from_config_file(filename)
-    print(len(todoist.uncompleted_tasks()))
+    print(len(todoist.uncompleted_tasks))
