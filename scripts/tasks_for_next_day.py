013ed72d01af59a564712c6461e0273d80b77151
Rutger van Beek <rdjvanbeek@gmail.com>
2021-01-29 21:53:17 +0100

    add support for weekly habits

--- scripts/tasks_for_next_day.py
+++ scripts/tasks_for_next_day.py
@@ -1,6 +1,6 @@
 from todoist_habit_tracker.todoist_connection import TodoistConnection
 from todoist_habit_tracker.habit import Habit
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 import pandas as pd
 import sys, getopt
@@ -11,44 +11,51 @@ def init_todoist():
     todoist = TodoistConnection.from_config_file(filename)
     return todoist
 
-
-def append_data(succes_dict):
+def append_data(succes_dict, weekly=False):
     dirname = os.path.abspath(os.path.abspath(__file__))
-    filename = os.path.join(os.path.split(dirname)[0], '../log/daily_tasks.csv')
+    filename = os.path.join(os.path.split(dirname)[0], '../log/{0}_tasks.csv'.format(('weekly' if weekly else 'daily')))
+    min_days = 7 if weekly else 1
+    index = (datetime.today() - timedelta(days=min_days)).strftime('%d/%m/%Y')
     try:
         df = pd.read_csv(filename, index_col=0)
-        df = df.append(pd.DataFrame(succes_dict, index=[datetime.today().strftime('%d/%m/%Y')]))
+        df = df.append(pd.DataFrame(succes_dict, index=[index]))
     except FileNotFoundError:
-        df = pd.DataFrame(succes_dict, index=[datetime.today().strftime('%d/%m/%Y')])
-        df.index.name = 'date'
+        df = pd.DataFrame(succes_dict, index=[index])
     df.to_csv(filename)
 
 
 def main(argv):
     test = False
     do_succes = False
+    weekly = False
     LABEL = 'habit_automatic'
+    WEEKLY_LABEL = 'upcoming_week'
     TEST_PROJECT='habit_tracker_test'
     try:
-        opts, args = getopt.getopt(argv, 'ts:')
+        opts, args = getopt.getopt(argv, 'tsw:')
     except getopt.GetoptError:
         sys.exit(2)
     for opt, args in opts:
         if opt == '-t':
             test = True
         if opt == '-s':
-            do_succes = args.lower() == "true"
+            do_succes = args.lower() == 'true'
+        if opt == '-w':
+            weekly = args.lower() == 'true'
     todoist = init_todoist()
     label_id = todoist.get_label_by_name(LABEL)['id']
+    weekly_label_id = todoist.get_label_by_name(WEEKLY_LABEL)['id']
     project_id = todoist.get_project_by_name(TEST_PROJECT)['id']
     if test:
         habits = todoist.filter_tasks(lambda task: (label_id in task['labels']) and (project_id == task['project_id']))
     else:
         habits = todoist.filter_tasks(lambda task: (label_id in task['labels']) and (project_id != task['project_id']))
-    habit_objects = [Habit(habit, todoist, label_id) for habit in habits]
+    habit_objects = [Habit(habit, todoist, label_id, weekly_label_id) for habit in habits]
+    habit_objects = filter(lambda habit: habit.weekly, habit_objects) if weekly \
+        else filter(lambda habit: habit.daily, habit_objects)
     succes = {}
     for habit in habit_objects:
-        succes[str(habit)] =  habit.done_yesterday()
+        succes[str(habit)] = habit.done()
         habit.determine_action()
 
     if do_succes: append_data(succes)
--- todoist_habit_tracker/habit.py
+++ todoist_habit_tracker/habit.py
@@ -3,17 +3,19 @@ import re
 
 class Habit:
 
-    def __init__(self, task, api, label_id):
+    def __init__(self, task, api, label_id, weekly_label_id):
         self.api = api
-        print(task['content'])
         effective_str = re.search('\[(.*)\]', task['content']).group(1)
         self.daily = 'daily' in effective_str
         self.later = 'later' in effective_str
+        self.weekly = 'weekly' in effective_str
         self.task = task
         self.content = re.sub('\[(.*)\]', '', self.task['content']).strip()
         self.label_id = label_id
+        self.weekly_label_id = weekly_label_id
 
     def create_new(self, due='today'):
+        print('here daily')
         task = {
             'content': self.content,
             'project_id': self.task['project_id'],
@@ -30,6 +32,24 @@ class Habit:
             new_task['parent_id'] = new['id']
             self.api.add_task(new_task)
 
+    def create_new_weekly(self):
+        print('here weekly')
+        task = {
+            'content': self.content,
+            'project_id': self.task['project_id'],
+            'priority': self.task['priority']
+        }
+        task['labels'] = [label for label in self.task['labels'] if label != self.label_id]
+        task['labels'].append(self.weekly_label_id)
+        task['parent_id'] = None
+        subtasks = self.api.filter_tasks(lambda x: self.task['id'] == x['parent_id'])
+        new = self.api.add_task(task)
+        for subtask in subtasks:
+            new_task = {'content': subtask['content'], 'project_id': subtask['project_id'],
+                        'priority': subtask['priority']}
+            new_task['parent_id'] = new['id']
+            self.api.add_task(new_task)
+
     def equal_tasks(self):
         def is_equal(task):
             return task['content'] == self.content and \
@@ -37,17 +57,22 @@ class Habit:
         return self.api.filter_tasks(is_equal)
 
     def determine_action(self):
-        flag = False
-        for task in self.equal_tasks():
-            if task['due'] == 'today':
-                flag = True
-            else:
-                if not self.later:
-                    self.api.delete_task(task)
-        if not flag:
-            self.create_new(due='today')
+        if self.daily:
+            flag = False
+            for task in self.equal_tasks():
+                if task['due'] == 'today':
+                    flag = True
+                else:
+                    if not self.later:
+                        self.api.delete_task(task)
+            if not flag:
+                self.create_new(due='today')
+        if self.weekly:
+            self.create_new_weekly()
 
-    def done_yesterday(self):
+    def done(self):
+        if self.weekly:
+            return False
         comp = self.api.completed_tasks
         contents = [re.sub('@.* ', '', c['content']) for c in comp]
         contents = [re.sub('@.*', '', c).strip() for c in contents]
